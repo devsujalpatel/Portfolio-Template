b856787249eff370832dc2b82139cdd39650bc14
Sujal Patel <sujal7455@gmail.com>
2025-06-22 21:28:15 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl font-bold">
+        Welcome To Sujal Patel Portfolio
+      </h1>
+    </div>
+  )
+}
+
+export default App
