b892011518b30a55da6c474b8ce7ee725d7734b7
Sujal Patel <sujal7455@gmail.com>
2025-06-23 23:12:21 +0530

    few changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { BrowserRouter as Router, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Routes } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <Router>
-    <Routes>
+  // <Router>
+  //   <Routes>
       <App />
-    </Routes>
-  </Router>
+  //   </Routes>
+  // </Router>
 );
