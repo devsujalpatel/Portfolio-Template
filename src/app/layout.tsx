c478d0ee138750dc6356f76b49e0573ae7e6be9f
sujalpatelcoder <sujal7455@gmail.com>
2025-07-03 02:29:17 +0530

    format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`} suppressHydrationWarning>
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable}`}
+      suppressHydrationWarning
+    >
       <head>
         <link rel="icon" href="/icon.svg" />
       </head>
