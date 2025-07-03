66a9a9b3dd904e193c21952d25f5c2b37f8a6453
sujalpatelcoder <sujal7455@gmail.com>
2025-07-03 16:31:58 +0530

    mini project section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,37 @@
 import Card from "@/components/Card";
 import { TextFlip } from "@/components/TextFlip";
 
+interface CardProps {
+  title: string;
+  description: string;
+  link: string;
+  image: string;
+}
+
+const cardProps: CardProps[] = [
+  {
+    title: "Vergin Mojito",
+    description: "A Restrount Showcase website for a restrount called Vergin Mojito",
+    link: "https://verginmojito.netlify.app",
+    image: "/images/mojito.png"
+  },
+  {
+    title: "Leet Ninja",
+    description: "A LeetCode like platform for developers to practice their coding skills",
+    link: "https://github.com/devsujalpatel/leetlab",
+    image:  "/images/mojito.png"
+  },
+  {
+    title: "Vibe It",
+    description: "An Ai Powered Web App Builder for vibe coders to create their own web apps",
+    link: "https://github.com/devsujalpatel/vibe-it",
+    image:  "/images/mojito.png"
+  },
+
+]
+
+
+
 export default function Home() {
   return (
     <div>
@@ -17,11 +48,21 @@ export default function Home() {
         </p>
       </div>
       
-      <div className="w-full h-full border-t mt-5 border-b shadow-neutral-900 shadow-inner">
-        <div className="m-5 border inline-block px-1 text-lg dark:bg-neutral-800 bg-neutral-300 dark:text-white">I love building things</div>
-        <div>
-          <Card  />
+      <div className="w-full h-full border-t mt-5 dark:shadow-neutral-950  shadow-inner">
+        <div className="m-5 border inline-block px-1 text-lg dark:bg-neutral-800 bg-neutral-100 dark:text-white">I love building things</div>
+        <div className="flex flex-wrap justify-center flex-row items-center gap-5">
+          {
+            cardProps.map((card: CardProps) => (
+              <Card key={card.title} title={card.title} description={card.description} link={card.link} image={card.image} />
+            ))
+          }
         </div>
+      </div>
+      <div className="w-full h-full border-t mt-5 dark:shadow-neutral-950  shadow-inner">
+        <div className="m-5 border inline-block px-1 text-lg dark:bg-neutral-800 bg-neutral-100 dark:text-white">Sharing knowledge as i learn</div>
+          <div>
+            <h3>What is </h3>
+          </div>
       </div>
     </div>
   );
